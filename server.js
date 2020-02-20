8a6252f059ce7d0e2f6aac3c415d8e9426be7f73
brendan hales <brendan_hales@hotmail.com>
2020-02-21 03:49:56 +0800

    setting correct database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const entryIncrament = require('./controllers/imageEntryCount');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'test',
-      database : 'smart_brain'
+      connectionString: 'process.env.DATABASE_URL',
+      ssl: true,
     }
   });
 
